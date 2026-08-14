8fc0473cee8d08f12710891aae4945c17aa09c06
Harshit Kumar <harshitsv2005@gmail.com>
2026-08-14 19:58:52 +0530

    Add resume update

--- server/index.js
+++ server/index.js
@@ -1,16 +1,11 @@
 import express from "express";
 import cors from "cors";
-
 const app = express();
-
 app.use(express.json());
 app.use(cors());
-
 app.get("/api/message", (req, res) => {
     res.json({ message: "Hello from the backend!" });
 })
-
-
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server is running 
     at http://localhost:${PORT}`));
